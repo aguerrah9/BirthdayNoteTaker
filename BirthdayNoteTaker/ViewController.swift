279d8d398c4cd722f1729f4d29d0ca8cd88d6c76
1140074 <alejandro.guerrah@elektra.com.mx>
2025-04-10 12:58:15 -0600

    Initial Commit

---
+++ BirthdayNoteTaker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayNoteTaker
+//
+//  Created by Alejandro Guerra Hernandez on 10/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
