9926498195da54adf454905d8ef03f34651971da
1140074 <alejandro.guerrah@elektra.com.mx>
2025-04-14 12:52:44 -0600

    layout final

--- BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var birthdayTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.value(forKey: "name")
+        let storedBirthday = UserDefaults.standard.value(forKey: "birthday")
+        
+        // Cast: as? vs as!
+        // Good!
+        nameTextField.text = storedName as? String
+        birthdayTextField.text = storedBirthday as? String
+        
+        // very good
+        if let myName = storedName as? String {
+            nameLabel.text = myName
+        }
+        if let myBirthday = storedBirthday as? String {
+            birthdayLabel.text = myBirthday
+        }
+        
     }
-
-
+    
+    @IBAction func saveClicked(_ sender: Any) {
+        
+        UserDefaults.standard.setValue(nameTextField.text!, forKey: "name")
+        UserDefaults.standard.setValue(birthdayTextField.text!, forKey: "birthday")
+        
+        nameLabel.text = "Name: \(nameTextField.text!)"
+        birthdayLabel.text = "Birthday: \(birthdayTextField.text!)"
+        
+    }
+    
+    @IBAction func deleteClicked(_ sender: Any) {
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let newName = storedName as? String {
+            UserDefaults.standard.removeObject(forKey: "name")
+        }
+        if let newBirthday = storedBirthday as? String {
+            UserDefaults.standard.removeObject(forKey: "birthday")
+        }
+        
+    }
 }
 
